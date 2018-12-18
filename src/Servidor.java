d87611a42b38e71313bc96c08923a46403009ed9
Francisco Araújo <franciscoaraujo@MacBook-Pro-de-Francisco.local>
2018-12-18 19:04:23 +0000

    orgganizado

---
+++ src/Servidor.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Servidor {
+
+    private int porta;
+    private int nClients;
+    ServidoresCloud servidoresCloud;
+    Utilizadores utilizadores;
+
+    public Servidor(int porta){
+        this.porta = porta;
+        this.nClients = 0;
+        this.servidoresCloud =new ServidoresCloud();
+        this.utilizadores=new Utilizadores();
+
+    }
+
+    public void comecaServidor(){
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(porta);
+
+            System.out.println("=================SERVER====================");
+
+            while (true) {
+
+                System.out.println("À espera de conexão....");
+
+                Socket clSocket = serverSocket.accept();
+
+                TratarCliente tratarCliente = new TratarCliente(clSocket,this.servidoresCloud,this.utilizadores);
+
+                Thread tratarClienteThread = new Thread(tratarCliente);
+
+                tratarClienteThread.setName("Cliente "+(this.nClients+1));
+
+                this.nClients++; // para fazer print
+
+                tratarClienteThread.start();
+
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String [] args){
+
+        int porta = 12345;
+        Servidor servidor = new Servidor(porta);
+        servidor.comecaServidor();
+    }
+}
